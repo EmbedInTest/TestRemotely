e372e01ae3a2328a27b04112df550da7ac12eb5c
Peter Buchegger <peter.buchegger7@gmail.com>
2024-01-01 12:02:43 +0000

    fix last pylint

--- PIORemotly/agent.py
+++ PIORemotly/agent.py
@@ -12,8 +12,9 @@ class Agent:
     def start(self, api: API) -> None:
         while not self._stop:
             if self._check_for_run(api):
-                run = self._get_run(api)
-                run.run()
+                pass
+                # run = self._get_run(api)
+                # run.run()
             time.sleep(1)
 
     def stop(self) -> None:
